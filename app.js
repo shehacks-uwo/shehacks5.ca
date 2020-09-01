c9d4df29faea3662d55f14e9acc3be1a49cec099
Jessie Wu <jessiewu@Jessies-Air.hitronhub.home>
2020-09-01 00:12:14 -0400

    full integration with fullpage and gsap libraries

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
-new fullpage('#fullpage', {
-	//options here
-    autoScrolling:true,
-    sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#000'],
-    navigation: true,
-    onLeave: (origin,destination,direction)=> {
-        const section = destination.item;
-        console.log(section);
-        TweenMax.fromTo(section, 1, {opacity: 0}, {opacity: 1},'+=700');
+new fullpage("#fullpage", {
+  //options here
+  autoScrolling: true,
+  onLeave: (origin, destination, direction) => {
+    const section = destination.item;
+    const storyText = section.querySelector("#tween-text");
+    const tl = new TimelineMax({ delay: 1.0 });
+
+    // python page animation
+    if (destination.index === 1) {
+      const pythonImg = document.querySelector("#python-img");
+      tl.fromTo(pythonImg, 0.5, { x: "-200%" }, { x: "15%" });
+      tl.fromTo(storyText, 1.0, { opacity: 0 }, { opacity: 1 });
     }
+
+    // java page animation
+    if (destination.index === 2) {
+      const javaImg = document.querySelector("#java-img");
+      tl.fromTo(javaImg, 0.5, { x: "-200%" }, { x: "15%" });
+      tl.fromTo(storyText, 1.0, { opacity: 0 }, { opacity: 1 });
+    }
+
+    // ruby page animation
+    if (destination.index === 3) {
+      const rubyImg = document.querySelector("#ruby-img");
+      tl.fromTo(rubyImg, 0.5, { x: "-200%" }, { x: "15%" });
+      tl.fromTo(storyText, 1.0, { opacity: 0 }, { opacity: 1 });
+    }
+  },
 });
 
+$("button").onClick(function () {
+  $("html,body").animate(
+    {
+      scrollTop: $(".second").offset().top,
+    },
+    "slow"
+  );
+});
