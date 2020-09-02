2ab3b0f7dbd7af88881d45e54abbb07f427bcbb3
Jessie Wu <jessiewu@Jessies-Air.hitronhub.home>
2020-09-02 13:34:28 -0400

    removed pins

--- app.js
+++ app.js
@@ -9,27 +9,39 @@ new fullpage("#fullpage", {
     // python page animation
     if (destination.index === 1) {
       const pythonImg = document.querySelector("#python-img");
-      tl.fromTo(pythonImg, 0.5, { x: "-200%" }, { x: "15%" });
+      tl.fromTo(pythonImg, 0.5, { x: "-100%" }, { x: "40%" });
       tl.fromTo(storyText, 1.0, { opacity: 0 }, { opacity: 1 });
     }
 
     // java page animation
     if (destination.index === 2) {
       const javaImg = document.querySelector("#java-img");
-      tl.fromTo(javaImg, 0.5, { x: "-200%" }, { x: "15%" });
+      tl.fromTo(javaImg, 0.5, { x: "-100%" }, { x: "40%" });
       tl.fromTo(storyText, 1.0, { opacity: 0 }, { opacity: 1 });
     }
 
     // ruby page animation
     if (destination.index === 3) {
       const rubyImg = document.querySelector("#ruby-img");
-      tl.fromTo(rubyImg, 0.5, { x: "-200%" }, { x: "15%" });
+      tl.fromTo(rubyImg, 0.5, { x: "-100%" }, { x: "40%" });
       tl.fromTo(storyText, 1.0, { opacity: 0 }, { opacity: 1 });
     }
   },
 });
 
-anime({
-  targets: ".pin",
-  translateX: 200,
+/*
+var waypoint = new Waypoint({
+  element: document.getElementById("pin-anim"),
+  handler: function () {
+    anime({
+      targets: "#pin-anim",
+      easing: "easeOutExpo",
+      translateX: [-100, 0],
+      opacity: [0, 1],
+      delay: 300,
+    });
+  },
+  context: document.querySelector(".section s2"),
+  offset: "100px",
 });
+*/
